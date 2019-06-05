8a64d411c90cc4ffde95503e80f697ca7fee25f3
Gerson Sanchez <gsanchez@latamready.com>
2019-06-04 22:09:17 -0500

    home profesor

---
+++ src/componentes/Profesor/homeProfesor.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class homeProfesor extends React.Component {
+
+  render() {
+    return (
+      <div class="wrapper pr-1 pl-1">
+        <nav id="sidebar">
+          <div class="sidebar-header pb-2 pt-2">
+            <img className="imagen" src="http://sistemas.unmsm.edu.pe/assets/fisi/img/logo/logo-fisi-header-2.png"></img>
+          </div>
+
+          <ul class="list-unstyle componenets">
+            <p>HachikoNight</p>
+            <li class = "active">
+              
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+
+}
+
+export default homeProfesor;
