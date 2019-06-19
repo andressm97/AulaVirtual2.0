63a9e7d6255e06ab3ec8b028d3cf28f8d1bcbc93
Gerson Sanchez <gsanchez@latamready.com>
2019-06-19 12:59:34 -0500

    la wea del profe csm

--- src/componentes/navbarProfesor.js
+++ src/componentes/navbarProfesor.js
@@ -1,23 +1,17 @@
 import React from 'react';
 
-class navbar extends React.Component {
+class navbarProfesor extends React.Component {
 
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-light mb-2 p-1 menu-color">
-
-                {/* <button type="button" id="sidebarCollapse" class="btn btn-info">
-                <i class="fa fa-align-justify"></i> <span>toggle sidebar</span>
-            </button> */}
-
-                {/* <!--<a class="navbar-brand" href="#">Navbar</a> --> */}
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href={"/home" + "/" + this.props.alumnoid}>Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href={"/homep" + "/" + this.props.teacherId}>Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">cursos</a>
@@ -30,8 +24,6 @@ class navbar extends React.Component {
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="/user">mis datos</a>
                                 <a class="dropdown-item" href="/">Cerrar Sesion</a>
-                                {/* <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Something else here</a> */}
                             </div>
                         </li>
                     </ul>
@@ -42,4 +34,4 @@ class navbar extends React.Component {
         )
     }
 }
-export default navbar
+export default navbarProfesor
